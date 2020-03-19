677ce2bf38a2722d00cd52487344d2a9ea386c2d
piotrxyz <piotr.zalom@gmail.com>
2020-03-19 23:29:47 +0100

    menu button implemented with menu list and svg icons

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,7 +1,7 @@
-const hamburger = document.querySelector('.nav__button');
+const hamburger = document.querySelector('.menu-button');
 const showMenu = document.querySelector('.menu');
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle('nav__button--active');
-    showMenu.classList.toggle('show-menu');
+    hamburger.classList.toggle('menu-button--active');
+    showMenu.classList.toggle('menu--show');
 })
